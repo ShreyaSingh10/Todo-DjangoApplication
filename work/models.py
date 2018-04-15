6b924b8a4f7a89486472faf7d5950645e5079bf8
ShreyaSingh10 <shreyasssingh10@gmail.com>
2018-04-15 15:10:16 +0530

    Second commit - Added superuser, added Todos Model

--- work/models.py
+++ work/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+from datetime import datetime
 
+class Todos(models.Model):
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_at = models.DateTimeField(default = datetime.now , blank = True)
+
+	def __str__(self):
+		return self.title
+	class Meta:
+		verbose_name_plural = "Todos"
+		#we did this to change the plural form to Posts
 
 # Create your models here.
