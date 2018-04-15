7a0fa1bfa02210fdeab32a348b008cf8b2bf975f
ShreyaSingh10 <shreyasssingh10@gmail.com>
2018-04-15 15:44:35 +0530

    Third commit - Added templates

--- work/views.py
+++ work/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todos
 
 def index(request):
-	return HttpResponse("Hey der! Let's do some work")
+	"""return HttpResponse("Hey der! Let's do some work")"""
+	todos = Todos.objects.all()[:10]
+	context = {
+		'todos': todos
+	}
+	return render(request, 'index.html', context)
