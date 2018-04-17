e3bb9f3954a96b3500e15ca6f9e5478c1489cff6
ShreyaSingh10 <shreyasssingh10@gmail.com>
2018-04-17 18:26:40 +0530

    Sixth Commit - Added delete API to support delete functionality

--- work/urls.py
+++ work/urls.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 
 urlpatterns = [
    url(r'^$', views.index , name ='index'),
-   url(r'^details/(?P<id>\d+)/$', views.details)
+   url(r'^details/(?P<id>\d+)/$', views.details),
+   url(r'^add/', views.add , name ='add'),
+   url(r'^delete/(?P<id>\d+)/$', views.delete, name='delete'),
 ]
 """ When we use url(r'^$', views.index, name='index') it means the url should be
  address/posts/  ^$ means include nothing, so its gona look ito our views.py file
--- work/views.py
+++ work/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Todos
 
@@ -17,6 +17,30 @@ def details(request, id):
 		'todos' :todos
 	}
 	return render(request, 'details.html', context)
+
+def add(request):
+	if(request.method == 'POST'):
+		title = request.POST['title']
+		text = request.POST['text']
+		todo = Todos(title=title, text=text)
+		todo.save()
+		print("in add")
+		return redirect('/work')
+	else:
+		return render(request, 'add.html')
+
+def delete(request, id):
+	if(request.method == 'POST'):
+		#print("in delete")
+		Todos.objects.filter(id = id).delete()
+		
+		return redirect('/work')
+	return render(request,'delete.html', {"id": id})
+		#todos.delete()	
+    
+	#return redirect('/work')
+	#else:
+		#return HttpResponse("Could not delete the post")
 
 """here Todos is the name  of the model we created"""
 """this id is same as the id we gave in urls.py"""
